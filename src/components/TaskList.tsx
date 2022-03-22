58890cc58e2a4821c71cf5614bc5d44000bfdcc0
Skywalker <ismael_junior@msn.com>
2022-03-21 23:04:09 -0300

    Primeiro commit desafio 1

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -13,17 +13,57 @@ interface Task {
 export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
+  ///criando um estado para se tornar o contador do id
+  let [counter,setCounter] = useState(0);
 
+  
+      
+  
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    //crio tipo igual a interface para receber os dados iniciais
+    setCounter(counter+1)
+
+    const createTask = {
+      id: counter,
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    //uso o spreed para atribuir valor ao setTask, porque ele add o valor anterior com o novo que estamos passando
+    setTasks(oldState => [...oldState , createTask])
+
+    //limpa o input
+    setNewTaskTitle('');
+
+
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+
+    //vamos tornar o checkbox enable
+    //atribuimos a variavel os resultados filtrados 
+    const enebleTask = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    }: task);
+
+    setTasks(enebleTask);
+    
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    ///filtramos todos os resultados que não foram selecionados
+    const tasksFilter = tasks.filter(task => task.id !== id)
+
+    //retorno as tasks que não foram selecionadas
+    setTasks(tasksFilter)    
+
   }
 
   return (
@@ -71,4 +111,6 @@ export function TaskList() {
       </main>
     </section>
   )
+  
+
 }
